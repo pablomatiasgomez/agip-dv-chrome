89af349e8d618ef3874e1399323172803700acfb
Pablo Matías Gomez <pablomatiasgomez@gmail.com>
2017-09-12 16:43:50 -0300

    triggering dv on text change (better for copy pasting..)

--- js/CommonDVPage.js
+++ js/CommonDVPage.js
@@ -1,19 +1,23 @@
 var CommonDVPage = function(options) {
-	if (!options.$id.length) return;
-	if (!options.$dv.length) return;
-	if (!options.$chkUseDv.length) return;
-	if (!options.dvGenerator) return;
+	if (!options.$id.length) throw "$id required";
+	if (!options.$dv.length) throw "$dv required";
+	if (!options.$chkUseDv.length) throw "$chkUseDv required";
+	if (!options.dvGenerator) throw "dvGenerator required";
+	if (!options.utils) throw "utils required";
+
+	var lastDv = null;
 
 	var bindEvents = function() {
-		options.$id.on("keyup", function() {
+		var fn = function() {
 			var dv = options.dvGenerator.getDV(options.$id.val());
-			if (!isNaN(dv)) {
+			if (!isNaN(dv) && dv !== lastDv) {
+				lastDv = dv;
 				options.$dv.val(dv);
-				if (options.utils) {
-					options.utils.trackGetCodePatente(options.$id.val() + "-" + dv);
-				}
+				options.utils.trackGetCodePatente(options.$id.val() + "-" + dv);
 			}
-		});
+		};
+		options.$id.on("keyup", fn);
+		options.$id.on("change", fn);
 	};
 
 	var checkCheckbox = function() {
