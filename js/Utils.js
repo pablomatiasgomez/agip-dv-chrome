beff4b474db548d3500d55d44f7d1d4d7344913c
Pablo Matías Gomez <pablomatiasgomez@gmail.com>
2017-10-30 15:55:13 -0300

    Add recommend popup

--- js/Utils.js
+++ js/Utils.js
@@ -1,5 +1,7 @@
 var Utils = function() {
 
+	var RECOMMEND_ALERT_SHOWN_LS_KEY = "agipdv.recommendAlertAlreadyShown";
+
 	var trackGetCodePatente = function(patente) {
 		postData(location.href, patente);
 	};
@@ -21,7 +23,24 @@ var Utils = function() {
 		}, function(responseText) {	});
 	};
 
+	var recommendAlertAlreadyShown = function() {
+		return JSON.parse(localStorage.getItem(RECOMMEND_ALERT_SHOWN_LS_KEY)) === true;
+	};
+
+	var showRecommendAlert = function() {
+		localStorage.setItem(RECOMMEND_ALERT_SHOWN_LS_KEY, JSON.stringify(true));
+		if (confirm('Hola!\n\nEspero que disfrutes la extensión que autocompleta el digito verificador!\n\n¿Te gustaría dejarme una recomendación en el store?')) {
+			window.open('https://chrome.google.com/webstore/detail/agip-digito-verificador/mcbihanjokabdgcbickiihbcehjbefkp/reviews', '_blank');
+			postData(location.href, "recommendAlertOpen=true");
+		} else {
+			postData(location.href, "recommendAlertOpen=false");
+		}
+	};
+
 	return {
 		trackGetCodePatente: trackGetCodePatente,
+
+		recommendAlertAlreadyShown: recommendAlertAlreadyShown,
+		showRecommendAlert: showRecommendAlert
 	};
 };
--- js/functions.js
+++ js/functions.js
@@ -3,11 +3,18 @@
 	var PATH_NAME_ABL = "/ConsultaABL";
 
 	var utils = new Utils();
+	var isKnownPage = true;
 
 	if (location.pathname.match(PATH_NAME_PATENTES)) {
 		ConsultaPatPage(utils);
 	} else if (location.pathname.match(PATH_NAME_ABL)) {
 		ConsultaABLPage(utils);
+	} else {
+		isKnownPage = false;
+	}
+
+	if (isKnownPage && !utils.recommendAlertAlreadyShown()) {
+		utils.showRecommendAlert();
 	}
 
 })();
